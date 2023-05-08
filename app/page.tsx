332cfb6955e87071e951c399606b62dc8cf98baa
emanuelefavero <emafaux89@gmail.com>
2023-05-08 20:45:31 +0200

    add variants

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 'use client'
 
+import { useState } from 'react'
 import { motion } from 'framer-motion'
 
+const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: '-100%' },
+}
+
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <>
       <h1 className='font-bold text-xl'>Framer Motion</h1>
+
       <h2>Keyframes</h2>
       <motion.div
         className='w-10 h-10 bg-indigo-500 rounded-full'
@@ -15,6 +24,24 @@ export default function Home() {
           borderRadius: ['20%', '20%', '50%', '50%', '20%'],
         }}
       />
+
+      <h2>Variants</h2>
+      <motion.nav
+        className='opacity-0 -translate-x-full'
+        animate={isOpen ? 'open' : 'closed'}
+        variants={variants}
+      >
+        <p>Item 1</p>
+        <p>Item 2</p>
+      </motion.nav>
+      <button
+        className='bg-indigo-600 text-white p-1'
+        onClick={() => {
+          setIsOpen(!isOpen)
+        }}
+      >
+        {isOpen ? 'Close' : 'Open'}
+      </button>
     </>
   )
 }
