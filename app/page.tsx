b6b903a20e519594e0b637580065fb41c8e187ae
emanuelefavero <emafaux89@gmail.com>
2023-05-08 20:35:28 +0200

    add keyframes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
 export default function Home() {
   return (
     <>
-      <h1>Hello</h1>
+      <h1 className='font-bold text-xl'>Framer Motion</h1>
+      <h2>Keyframes</h2>
+      <motion.div
+        className='w-10 h-10 bg-indigo-500 rounded-full'
+        animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ['20%', '20%', '50%', '50%', '20%'],
+        }}
+      />
     </>
   )
 }
